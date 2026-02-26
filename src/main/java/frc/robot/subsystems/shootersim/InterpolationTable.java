d0b261b2e083822de9e19e2e5a878be397857c95
fossil-fishhooks <arin.jander@student.csd509j.net>
2026-02-25 18:46:08 -0800

    start work on shooter sim (committing to put files)

---
+++ src/main/java/frc/robot/subsystems/shootersim/InterpolationTable.java
@@ -0,0 +1,4 @@
+package frc.robot.subsystems.shootersim;
+
+public class InterpolationTable {
+}
---
+++ src/main/java/frc/robot/subsystems/shootersim/ShooterSim.java
@@ -0,0 +1,13 @@
+package frc.robot.subsystems.shootersim;
+
+public class ShooterSim {
+    private double SHOOTER_X = 0.5; //offset from middle of robot
+    private double SHOOTER_Y = 0;
+    private double SHOOTER_Z = 0.4;
+    private double SHOOTER_LAUNCH_THETA = 2*Math.PI/5; //Radians from horiz!
+
+
+    private void SimShot(){
+
+    }
+}
