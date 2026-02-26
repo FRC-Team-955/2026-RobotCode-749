b9bdb4380414ce135c08de4a215597de06547c57
fossil-fishhooks <arin.jander@student.csd509j.net>
2026-02-25 21:18:32 -0800

    start work on ShooterSim

--- src/main/java/frc/robot/subsystems/shootersim/ShooterSim.java
+++ src/main/java/frc/robot/subsystems/shootersim/ShooterSim.java
@@ -5,6 +5,11 @@ public class ShooterSim {
     private double SHOOTER_Y = 0;
     private double SHOOTER_Z = 0.4;
     private double SHOOTER_LAUNCH_THETA = 2*Math.PI/5; //Radians from horiz!
+    InterpolationTable Table = new InterpolationTable(); // this thing is meant to track voltage vs landing point!
+    public ShooterSim(){
+        Table.add(0,0);
+        Table.add(12,15); //meters
+    }
 
 
     private void SimShot(){
