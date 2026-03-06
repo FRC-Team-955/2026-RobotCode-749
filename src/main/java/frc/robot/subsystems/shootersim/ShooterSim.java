d4faee53f9669da4672f0a99871d91dd0ba07670
fossil-fishhooks <arin.jander@student.csd509j.net>
2026-03-05 18:49:13 -0800

    tune table

--- src/main/java/frc/robot/subsystems/shootersim/ShooterSim.java
+++ src/main/java/frc/robot/subsystems/shootersim/ShooterSim.java
@@ -18,9 +18,9 @@ public class ShooterSim {
     InterpolationTable AngularVelocityLaunchAngle = new InterpolationTable(); // track angular velocity -> launch angle
     public ShooterSim(){
         AngularVelocityToLandingPt.add(0,0);
-        AngularVelocityToLandingPt.add(38,2.48); //107 in
-        AngularVelocityToLandingPt.add(52,3.86); //meters!  160 in
-        AngularVelocityToLandingPt.add(65,4.58); // THIS ISNT TUNED!
+        AngularVelocityToLandingPt.add(37,2.48); //107 in
+        AngularVelocityToLandingPt.add(50,3.86); //meters!  160 in
+        AngularVelocityToLandingPt.add(63,4.58); // THIS ISNT TUNED!
 
         AngularVelocityLaunchAngle.add(0,Math.PI/2);
         AngularVelocityLaunchAngle.add(40,Math.PI/2-Math.toRadians(30)); //
